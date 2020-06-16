ccd81df6e4d522b62cdfaa8ffe2c7485f22ccd65
Peter Shen <peterchenshen@gmail.com>
2020-06-16 11:26:22 +0800

    add log component

---
+++ src/log.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "log.h"
+
+static loglevel_t s_level = LOG_LV_DEBUG;
+
+int log_setlevel(loglevel_t level)
+{
+    if (level >= LOG_LV_MAX)
+    {
+        return -1;
+    }
+
+    s_level = level;
+    return 0;
+}
+
+int log_printf(loglevel_t level, const char *fmt, ...)
+{
+    int n;
+    va_list ap;
+
+    if (level > s_level)
+    {
+        return 0;
+    }
+
+    va_start(ap, fmt);
+    n = vprintf(fmt, ap);
+    va_end(ap);
+
+    return n;
+}
